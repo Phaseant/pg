16d90c9490691a268afccc478a910c7ceb77a226
Klim Krivoguzov <krivoguzov.klim@yandex.ru>
2023-06-12 21:04:53 +0300

    Deleted generated comments

--- cmd/root.go
+++ cmd/root.go
@@ -15,9 +15,6 @@ var rootCmd = &cobra.Command{
 	Use:   "pg",
 	Short: "Password Generator is a CLI tool to generate passwords",
 	Long:  `Password Generator is a CLI tool to generate passwords. You can use it to generate passwords with different lengths and different characters.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -30,13 +27,5 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.PasswordGenerator.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
 	rootCmd.CompletionOptions.DisableDefaultCmd = true
 }
